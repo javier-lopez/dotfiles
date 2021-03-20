7c0c96fc67aa4e3604258546975250018c372a2e
Javier López <javier-lopez@users.noreply.github.com>
2021-03-20 01:39:04 -0600

    profile.ps1: add help comments

--- .profile.ps1
+++ .profile.ps1
@@ -1,5 +1,9 @@
 # Put this into: $ notepad $profile
 
+# If powershell outputs an error execute any of the following commands from a privileged shell:
+# $ Set-ExecutionPolicy Unrestricted
+# $ Set-ExecutionPolicy RemoteSigned 
+
 Set-PSReadlineOption -BellStyle None
 #Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineOption -EditMode Vi #this is sparta!
