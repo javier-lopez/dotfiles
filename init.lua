9cc01d7e33eadcf6f9044179ebda46b28e504d80
Javier López <javier-lopez@users.noreply.github.com>
2025-02-06 14:55:02 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -60,7 +60,7 @@ require("lazy").setup({ -- :Lazy
     { "stevearc/dressing.nvim", event = "VeryLazy" }, -- improve default vim.ui interfaces
 
     { "LunarVim/bigfile.nvim", -- huge files optimizer, disable expansive vim features
-        config = function()
+        config = function()    --- @diagnostic disable-next-line: missing-fields
             require("bigfile").setup({
                 size = 10, -- Size in MB
                 disable = { "markdown", "json", "yaml", "vim", "lua", "typescript", "javascript" },
@@ -72,7 +72,7 @@ require("lazy").setup({ -- :Lazy
         dependencies = "nvim-lua/plenary.nvim",
         config = true,
         keys = { -- load the plugin only when using it's keybinding:
-            { "<leader>u", "<cmd>lua require('undotree').toggle()<CR>" },
+            { "<leader>u", "<cmd>lua require('undotree').toggle()<CR>", desc="UndoTree" },
         },
     },
 
@@ -867,6 +867,47 @@ require("lazy").setup({ -- :Lazy
         --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
         --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     },
+
+    {   "piersolenski/wtf.nvim", -- Debug diagnostic with AI
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {},
+        keys = {
+            {
+                "<leader>wa",
+                mode = { "n", "x" },
+                function()
+                    require("wtf").ai()
+                end,
+                desc = "Debug diagnostic with AI",
+            },
+            {
+                mode = { "n" },
+                "<leader>ws",
+                function()
+                    require("wtf").search()
+                end,
+                desc = "Search diagnostic with Google",
+            },
+            {
+                mode = { "n" },
+                "<leader>wh",
+                function()
+                    require("wtf").history()
+                end,
+                desc = "Populate the quickfix list with previous chat history",
+            },
+            {
+                mode = { "n" },
+                "<leader>wg",
+                function()
+                    require("wtf").grep_history()
+                end,
+                desc = "Grep previous chat history with Telescope",
+            },
+        },
+    }
 ---@diagnostic disable-next-line: missing-fields
 }, {
         ui = {
